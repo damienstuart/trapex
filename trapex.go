5134ebf327f065f6d68dbb39b1d790c6d998ec5f
Damien Stuart <dstuart@dstuart.org>
2019-11-04 11:58:00 -0500

    Initial commit of working (but not nearly finished) code.

---
+++ trapex.go
@@ -0,0 +1,225 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	g "github.com/damienstuart/gosnmp"
+)
+
+var listenAddr string = "0.0.0.0:9162"
+
+var trapDestList = []string{
+	"192.168.7.12:162",
+}
+
+// teStats is a structure for holding trapex stats.
+type teStats struct {
+	startTime     uint32
+	trapCount     uint64
+	filteredTraps uint64
+	fromV2c       uint64
+	fromV3        uint64
+}
+
+var stats teStats
+
+// Trap destinations
+var trapDests []*g.GoSNMP
+
+// sgTrap holds a pointer to a trap and the source IP of
+// the incoming trap.
+//
+type sgTrap struct {
+	data       g.SnmpTrap
+	trapVer    g.SnmpVersion
+	srcIP      net.IP
+	translated bool
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Printf("Usage:\n")
+		fmt.Printf("   %s\n", filepath.Base(os.Args[0]))
+		flag.PrintDefaults()
+	}
+
+	//stats.trapCount = 0
+	//stats.filteredTraps = 0
+
+	tl := g.NewTrapListener()
+
+	tl.OnNewTrap = trapHandler
+	tl.Params = g.Default
+	tl.Params.Community = ""
+
+	// Uncomment for debugging gosnmp
+	//tl.Params.Logger = log.New(os.Stdout, "", 0)
+
+	// SNMP v3 stuff
+	tl.Params.SecurityModel = g.UserSecurityModel
+	tl.Params.MsgFlags = g.AuthPriv
+	tl.Params.Version = g.Version3
+	tl.Params.SecurityParameters = &g.UsmSecurityParameters{
+		UserName:                 "sguser",
+		AuthenticationProtocol:   g.SHA,
+		AuthenticationPassphrase: "sgpassword",
+		//PrivacyProtocol:          g.DES,
+		PrivacyProtocol:   g.AES,
+		PrivacyPassphrase: "sgpassword",
+	}
+
+	makeTrapDests()
+
+	fmt.Println("Start trapex listener on " + listenAddr)
+	err := tl.Listen(listenAddr)
+	if err != nil {
+		log.Panicf("error in listen: %s", err)
+	}
+}
+
+func trapHandler(p *g.SnmpPacket, addr *net.UDPAddr) {
+	stats.trapCount++
+	/*
+		if filteredTrap(p, addr.IP) {
+			//fmt.Printf("*** Filtered trap %s from %s\n", p.Enterprise, addr.IP)
+			trapsFiltered++
+			return
+		}
+	*/
+
+	// Make the trap
+	trap := sgTrap{
+		data: g.SnmpTrap{
+			Variables:    p.Variables,
+			Enterprise:   p.Enterprise,
+			AgentAddress: p.AgentAddress,
+			GenericTrap:  p.GenericTrap,
+			SpecificTrap: p.SpecificTrap,
+			Timestamp:    p.Timestamp,
+		},
+		srcIP:   addr.IP,
+		trapVer: p.Version,
+	}
+
+	// Translate to v1 if needed
+	/*
+	 */
+	if p.Version > g.Version1 {
+		err := translateToV1(&trap)
+		if err != nil {
+			fmt.Printf("Error translating to v1: %v\n", err)
+		}
+	}
+
+	logTrap(&trap)
+
+	go forwardTrap(&trap)
+}
+
+// forwardTrap sends the incoming trap to the configured trap destinations.
+//
+func forwardTrap(trap *sgTrap) {
+	for _, td := range trapDests {
+		_, err := td.SendTrap(trap.data)
+		if err != nil {
+			fmt.Printf("SendTrap() error sending to %s: trap# %v: %v\n", td.Target, stats.trapCount, err)
+		}
+	}
+}
+
+// logTrap (for now) prints to stdout - a format that mimics the current
+// SG trapexploder log file format.
+//
+func logTrap(t *sgTrap) {
+	trap := &t.data
+
+	fmt.Printf("\nTrap: %v", stats.trapCount)
+	if t.translated == true {
+		fmt.Printf(" (translated from v%s)", t.trapVer.String())
+	}
+	fmt.Printf("\n\t%s\n", time.Now().Format(time.ANSIC))
+	fmt.Printf("\tSrc IP: %s\n", t.srcIP)
+	fmt.Printf("\tAgent: %s\n", trap.AgentAddress)
+	//fmt.Printf("\tVersion: %s\n", t.trapVer.String())
+	fmt.Printf("\tTrap Type: %v\n", trap.GenericTrap)
+	fmt.Printf("\tSpecific Type: %v\n", trap.SpecificTrap)
+	fmt.Printf("\tEnterprise: %s\n", strings.Trim(trap.Enterprise, "."))
+	fmt.Printf("\tTimestamp: %v\n", trap.Timestamp)
+
+	// Process the Varbinds.
+	for _, v := range trap.Variables {
+		vbName := strings.Trim(v.Name, ".")
+		switch v.Type {
+		case g.OctetString:
+			//b := v.Value.([]byte)
+			//fmt.Printf("\tObject:%s Value:%s\n", vbName, cleanOctets(b))
+			fmt.Printf("\tObject:%s Value:%s\n", vbName, string(v.Value.([]byte)))
+		default:
+			fmt.Printf("\tObject:%s Value:%v\n", vbName, v.Value)
+		}
+	}
+}
+
+// makeTrapDests populates the trapDest array with the list
+// of trap destinations and ports.
+//
+func makeTrapDests() {
+	tDests := make([]*g.GoSNMP, len(trapDestList))
+	for i, d := range trapDestList {
+		s := strings.Split(d, ":")
+		port, err := strconv.Atoi(s[1])
+		if err != nil {
+			panic("Invalid destination port: " + d)
+		}
+		td := &g.GoSNMP{
+			Target:             s[0],
+			Port:               uint16(port),
+			Transport:          "udp",
+			Community:          "",
+			Version:            g.Version1,
+			Timeout:            time.Duration(2) * time.Second,
+			Retries:            3,
+			ExponentialTimeout: true,
+			MaxOids:            g.MaxOids,
+		}
+		err = td.Connect()
+		if err != nil {
+			panic(err)
+		}
+		tDests[i] = td
+		trapDests = tDests
+		fmt.Printf("--Added trap destination: %s, port %s\n", s[0], s[1])
+	}
+}
+
+// -DSS Temp - filteredTrap checks the incoming trap against various filters and will
+// return true if the trap should be filtered (ignored)
+func filteredTrap(p *g.SnmpPacket, ip net.IP) bool {
+	if len(p.Enterprise) > 1 && strings.Trim(p.Enterprise, ".") == "1.3.6.1.4.1.546.1.1" {
+		return true
+	}
+	return false
+}
+
+// cleanOctets takes an array of bytes and removes non-ascii (or rather
+// printable) characters. It will allow for tab and newline characters
+// however. The result is returned as a string.
+//
+func cleanOctets(indat []byte) string {
+	n := 0
+	for _, c := range indat {
+		if c >= 32 && c < 127 && c != 10 && c != 9 {
+			indat[n] = c
+			n++
+		}
+	}
+	return string(indat[:n])
+}
---
+++ v2c_to_v1.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	g "github.com/damienstuart/gosnmp"
+)
+
+// OID constants we will need for v2c to v1 conversion.
+const (
+	snmpTraps          = ".1.3.6.1.6.3.1.1.5"
+	snmpTrapOID        = ".1.3.6.1.6.3.1.1.4.1.0"
+	snmpTrapEnterprise = ".1.3.6.1.6.3.1.1.4.3.0"
+	snmpTrapAddress    = ".1.3.6.1.6.3.18.1.3.0"
+	sysUpTime          = ".1.3.6.1.2.1.1.3.0"
+)
+
+// translateToV1 converts a trap from v2c/v3 to v1 per RFC-3584
+//
+func translateToV1(t *sgTrap) error {
+	// If this is already a v1 trap, there is nothing to do.
+	if t.trapVer == g.Version1 {
+		return nil
+	}
+
+	trap := &t.data
+	vb0 := trap.Variables[0]
+	vb1 := trap.Variables[1]
+
+	// We expect varbind 0 to be sysUptime (type: TimeTicks). If it isn't,
+	// something is wrong and we bail.
+	//
+	if vb0.Name != sysUpTime || vb0.Type != g.TimeTicks {
+		return fmt.Errorf("Invalid sysUptime (varbind0) for v2c trap: %v", vb0)
+	}
+	// We also expect varbind 1 to the snmpTrapOID.
+	if vb1.Name != snmpTrapOID || vb1.Type != g.ObjectIdentifier {
+		return fmt.Errorf("Invalid snmpTrapOID (varbind1) for v2c trap: %v", vb0)
+	}
+
+	// Use the sysUpTime varbind value for v1 timestamp
+	trap.Timestamp = uint(vb0.Value.(uint32))
+
+	trapOID := vb1.Value.(string)
+
+	// Let's see if we are dealing with a standard trap
+	var isStd bool
+	if strings.HasPrefix(trapOID, snmpTraps) {
+		isStd = true
+		// Remove any trailing .0 from the trapOID value
+		if strings.HasSuffix(trapOID, ".0") {
+			trapOID = trapOID[:len(trapOID)-2]
+		}
+	}
+
+	// Get the the position of the last "." in the trapOID so we can use that
+	// as our first demarcation to parse out the last OID element.
+	dmarc := strings.LastIndex(trapOID, ".")
+
+	// Using the dmarc, we get the value of the last element of trapOID
+	jval, err := strconv.Atoi(trapOID[dmarc+1:])
+	if err != nil {
+		return fmt.Errorf("Error parsing last element of trapOID: %s", trapOID)
+	}
+
+	// For standard traps, generic trap type will be set to the value
+	// of the last element - 1 , and specific will be 0.
+	if isStd == true {
+		trap.GenericTrap = jval - 1
+		trap.SpecificTrap = 0
+		// Otherwise
+	} else {
+		trap.GenericTrap = 6
+		trap.SpecificTrap = jval
+		// Also if the next to last element in trapOID is "0", then we
+		// move the demarc to the next previous "." so we can discard
+		// the last to elements in case we need to use it for the Enterprise
+		// value.
+		ndx := strings.LastIndex(trapOID[:dmarc], ".")
+		if trapOID[ndx+1:dmarc] == "0" {
+			dmarc = ndx
+		}
+	}
+
+	// Now process the varbinds: Skip the first 2 and remove any Counter64
+	// types. We may be capturing the Enterprise and AgentAddress value
+	// from the varbinds as well. If not, we have fallbacks for them later.
+	var enterprise string
+	var agentAddress string
+	n := 0
+	for i, v := range trap.Variables {
+		// Skip the first 2 varbinds as we don't need them in the v1 trap.
+		if i < 2 {
+			continue
+		}
+		// Ignore/Skip any Counter64 types as they are not supported in v1.
+		if v.Type != g.Counter64 {
+			trap.Variables[n] = v
+			n++
+		}
+		// If this is a standard trap and the snmpTrapEnterprise OID, set
+		// the v1 Enterprise value accordingly
+		if isStd && v.Name == snmpTrapEnterprise {
+			enterprise = v.Value.(string)
+			// Or if we have an snmpTrapAddress OID set agentAddress to its value
+		} else if v.Name == snmpTrapAddress {
+			agentAddress = v.Value.(string)
+		}
+	}
+	trap.Variables = trap.Variables[:n]
+
+	// Figure out what our Enterprise value should be.
+	if len(enterprise) > 0 {
+		trap.Enterprise = enterprise
+	} else if isStd {
+		trap.Enterprise = snmpTraps
+	} else {
+		trap.Enterprise = trapOID[:dmarc]
+	}
+
+	// Now set the Agent address. If it is not in the trap data, then use the
+	// packet source IP.
+	if len(agentAddress) > 0 {
+		trap.AgentAddress = agentAddress
+	} else {
+		trap.AgentAddress = t.srcIP.String()
+	}
+
+	t.translated = true
+
+	// Update the translate stats
+	if t.trapVer == g.Version2c {
+		stats.fromV2c++
+	} else if t.trapVer == g.Version3 {
+		stats.fromV3++
+	}
+
+	return nil
+}
