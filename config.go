e7321a73f880eae0a355eadf1585503c049982df
Damien Stuart <dstuart@dstuart.org>
2021-08-01 16:40:45 -0400

    Added license text to source files.

--- config.go
+++ config.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2021 Damien Stuart. All rights reserved.
+//
+// Use of this source code is governed by the MIT License that can be found
+// in the LICENSE file.
+//
 package main
 
 import (
