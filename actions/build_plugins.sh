152ed38c31f38dfe277758a518c1de97fc96f594
Damien Stuart <dstuart@dstuart.org>
2022-01-01 13:44:35 -0500

    Tweaks to Makefile and build_plugins.sh script

--- actions/build_plugins.sh
+++ actions/build_plugins.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "Building plugins:"
 for source in `ls -1 plugins/*.go`; do
     plugin=`echo $source | sed -e 's/\.go$//'`
+    echo " - plugin: $plugin"
     go build -buildmode=plugin -o $plugin.so $plugin.go
 done
 
