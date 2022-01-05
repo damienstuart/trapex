4c44793ae67f36eed0c1650cfb3d3581a31225b4
Kells Kearney <keruzu@gmail.com>
2022-01-05 16:10:40 -0500

    Better support unit testing and docs for plugins

--- actions/build_plugins.sh
+++ actions/build_plugins.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 echo "Building plugins:"
-for source in `ls -1 plugins/*.go`; do
-    plugin=`echo $source | sed -e 's/\.go$//'`
+for plugin in `ls -1 plugins`; do
     echo " - plugin: $plugin"
-    go build -buildmode=plugin -o $plugin.so $plugin.go
+    (cd plugins/$plugin && go build -buildmode=plugin -o ../$plugin.so $plugin.go)
 done
 
