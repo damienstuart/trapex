3a08bc43fb518f33e15f623eca25e9342348991e
Kells <keruzu@users.noreply.github.com>
2022-01-16 10:17:58 -0500

    Revert "Feature/filter plugins"

--- filter.go
+++ filter.go
@@ -6,12 +6,16 @@
 package main
 
 import (
+	"log"
 	"net"
+	"os"
 	"regexp"
+	"strconv"
 	"strings"
+	"time"
 
-	pluginMeta "github.com/damienstuart/trapex/txPlugins"
-	pluginLoader "github.com/damienstuart/trapex/txPlugins/interfaces"
+	g "github.com/gosnmp/gosnmp"
+	"github.com/natefinch/lumberjack"
 )
 
 // Filter types
@@ -27,51 +31,204 @@ const (
 
 // Filter object items
 const (
-	filterByVersion int = iota
-	filterBySrcIP
-	filterByAgentAddr
-	filterByGenericType
-	filterBySpecificType
-	filterByOid
+	version int = iota
+	srcIP
+	agentAddr
+	genericType
+	specificType
+	enterprise
 )
 
 // Supported action types
 const (
 	actionBreak int = iota
 	actionNat
-	actionPlugin
+	actionForward
+	actionForwardBreak
+	actionLog
+	actionLogBreak
+	actionCsv
+	actionCsvBreak
 )
+
+// filterObj represents one of the filterable items in a filter line from
+// the config file (i.e. Src IP, AgentAddress, GenericType, SpecificType,
+// and Enterprise OID).
+//
+type filterObj struct {
+	filterItem  int
+	filterType  int
+	filterValue interface{} // string, *regex.Regexp, *network, int
+}
+
+// trapexFilter holds the filter data and action for a specfic
+// filter line from the config file.
+type trapexFilter struct {
+	filterItems []filterObj
+	matchAll    bool
+	action      interface{}
+	actionType  int
+	actionArg   string
+}
+
+// trapForwarder is an instance of a forward destination.
+//
+type trapForwarder struct {
+	destination *g.GoSNMP
+}
+
+// trapLogger is an instace of a trap logfile destination.
+//
+type trapLogger struct {
+	logFile   string
+	fd        *os.File
+	logHandle *log.Logger
+	isBroken  bool
+}
+
+// trapCsvLogger is an instace of a trap CSV logfile destination.
+//
+type trapCsvLogger struct {
+	logFile   string
+	fd        *os.File
+	logger    *lumberjack.Logger
+	logHandle *log.Logger
+	isBroken  bool
+}
+
+// Initialize a trapForwarder instance.
+//
+func (a *trapForwarder) initAction(dest string) error {
+	s := strings.Split(dest, ":")
+	port, err := strconv.Atoi(s[1])
+	if err != nil {
+		panic("Invalid destination port: " + s[1])
+	}
+	a.destination = &g.GoSNMP{
+		Target:             s[0],
+		Port:               uint16(port),
+		Transport:          "udp",
+		Community:          "",
+		Version:            g.Version1,
+		Timeout:            time.Duration(2) * time.Second,
+		Retries:            3,
+		ExponentialTimeout: true,
+		MaxOids:            g.MaxOids,
+	}
+	err = a.destination.Connect()
+	if err != nil {
+		return (err)
+	}
+	logger.Info().Str("target", s[0]).Str("port", s[1]).Msg("Added trap destination")
+	return nil
+}
+
+// Hook for sending a trap to the destination defined for this trapForwarder
+// instance.
+//
+func (a trapForwarder) processTrap(trap *sgTrap) error {
+	_, err := a.destination.SendTrap(trap.data)
+	return err
+}
+
+// Close the trapForwarder connection
+//
+func (a trapForwarder) close() {
+	a.destination.Conn.Close()
+}
+
+// Initialize a trapLogger instance.
+//
+func (a *trapLogger) initAction(logfile string, teConf *trapexConfig) error {
+	fd, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	a.fd = fd
+	a.logFile = logfile
+	a.logHandle = log.New(fd, "", 0)
+	a.logHandle.SetOutput(makeLogger(logfile, teConf))
+	logger.Info().Str("logfile", logfile).Msg("Added log destination")
+	return nil
+}
+
+// Hook for logging a trap for this instance of a log action.
+//
+func (a *trapLogger) processTrap(trap *sgTrap) {
+	logTrap(trap, a.logHandle)
+}
+
+// Close a trap logger handle
+//
+func (a *trapLogger) close() {
+	a.fd.Close()
+}
+
+// Initialize a trapCsvLogger instance.
+//
+func (a *trapCsvLogger) initAction(logfile string, teConf *trapexConfig) error {
+	fd, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	a.fd = fd
+	a.logFile = logfile
+	a.logHandle = log.New(fd, "", 0)
+	a.logger = makeCsvLogger(logfile, teConf)
+	a.logHandle.SetOutput(a.logger)
+	logger.Info().Str("logfile", logfile).Msg("Added CSV log destination")
+	return nil
+}
+
+// Hook for logging a trap for this instance of a log action.
+//
+func (a *trapCsvLogger) processTrap(trap *sgTrap) {
+	logCsvTrap(trap, a.logHandle)
+}
+
+// Get this logger's file name
+func (a *trapCsvLogger) logfileName() string {
+	return a.logFile
+}
+
+// Force a CSV log rotation
+func (a *trapCsvLogger) rotateLog() {
+	a.logger.Rotate()
+}
+
+// Close a trap logger handle
+//
+func (a *trapCsvLogger) close() {
+	a.fd.Close()
+}
 
 // isFilterMatch checks trap data against a trapexFilter and returns a boolean
 // to indicate whether or not the trap data matches the filter criteria.
 //
-func (f *trapexFilter) isFilterMatch(sgt *pluginMeta.Trap) bool {
-	if len(f.matchers) == 0 {
-		return true
-	}
+func (f *trapexFilter) isFilterMatch(sgt *sgTrap) bool {
 	// Assume true - until one of the filter items does not match
-	trap := &(sgt.Data)
-	for _, fo := range f.matchers {
+	trap := &(sgt.data)
+	for _, fo := range f.filterItems {
 		fval := fo.filterValue
 		switch fo.filterItem {
-		case filterByVersion:
-			if fval != sgt.SnmpVersion {
+		case version:
+			if fval != sgt.trapVer {
 				return false
 			}
-		case filterBySrcIP:
-			if fo.filterType == parseTypeString && fval.(string) != sgt.SrcIP.String() {
+		case srcIP:
+			if fo.filterType == parseTypeString && fval.(string) != sgt.srcIP.String() {
 				return false
-			} else if fo.filterType == parseTypeCIDR && !fval.(*network).contains(sgt.SrcIP) {
+			} else if fo.filterType == parseTypeCIDR && !fval.(*network).contains(sgt.srcIP) {
 				return false
-			} else if fo.filterType == parseTypeRegex && !fval.(*regexp.Regexp).MatchString(sgt.SrcIP.String()) {
+			} else if fo.filterType == parseTypeRegex && !fval.(*regexp.Regexp).MatchString(sgt.srcIP.String()) {
 				return false
 			} else if fo.filterType == parseTypeIPSet {
-				_, ok := teConfig.IpSets[fval.(string)][sgt.SrcIP.String()]
+				_, ok := teConfig.ipSets[fval.(string)][sgt.srcIP.String()]
 				if ok != true {
 					return false
 				}
 			}
-		case filterByAgentAddr:
+		case agentAddr:
 			if fo.filterType == parseTypeString && fval.(string) != trap.AgentAddress {
 				return false
 			} else if fo.filterType == parseTypeCIDR && !fval.(*network).contains(net.ParseIP(trap.AgentAddress)) {
@@ -79,22 +236,22 @@ func (f *trapexFilter) isFilterMatch(sgt *pluginMeta.Trap) bool {
 			} else if fo.filterType == parseTypeRegex && !fval.(*regexp.Regexp).MatchString(trap.AgentAddress) {
 				return false
 			} else if fo.filterType == parseTypeIPSet {
-				_, ok := teConfig.IpSets[fval.(string)][trap.AgentAddress]
+				_, ok := teConfig.ipSets[fval.(string)][trap.AgentAddress]
 				if ok != true {
 					return false
 				}
 			}
-		case filterByOid:
+		case enterprise:
 			if fo.filterType == parseTypeRegex && !fval.(*regexp.Regexp).MatchString(strings.TrimLeft(trap.Enterprise, ".")) {
 				return false
 			} else if fo.filterType == parseTypeString && fval.(string) != strings.TrimLeft(trap.Enterprise, ".") {
 				return false
 			}
-		case filterByGenericType:
+		case genericType:
 			if fo.filterType == parseTypeInt && fval.(int) != trap.GenericTrap {
 				return false
 			}
-		case filterBySpecificType:
+		case specificType:
 			if fo.filterType == parseTypeInt && fval.(int) != trap.SpecificTrap {
 				return false
 			}
@@ -106,28 +263,42 @@ func (f *trapexFilter) isFilterMatch(sgt *pluginMeta.Trap) bool {
 // processAction handles the execution of the action for the
 // trapexFilter instance on the the given trap data.
 //
-func (f *trapexFilter) processAction(trap *pluginMeta.Trap) error {
-	var err error
-
+func (f *trapexFilter) processAction(sgt *sgTrap) {
 	switch f.actionType {
 	case actionBreak:
-		trap.Dropped = true
+		sgt.dropped = true
+		return
 	case actionNat:
-		if f.ActionArg == "$SRC_IP" {
-			trap.Data.AgentAddress = trap.SrcIP.String()
+		if f.actionArg == "$SRC_IP" {
+			sgt.data.AgentAddress = sgt.srcIP.String()
 		} else {
-			trap.Data.AgentAddress = f.ActionArg
+			sgt.data.AgentAddress = f.actionArg
 		}
-	case actionPlugin:
-		err = f.plugin.(pluginLoader.ActionPlugin).ProcessTrap(trap)
-		if err != nil {
-			trapexLog.Err(err).Str("plugin", f.ActionName).Msg("Issue in processing trap by plugin")
+	case actionForward:
+		f.action.(*trapForwarder).processTrap(sgt)
+	case actionForwardBreak:
+		f.action.(*trapForwarder).processTrap(sgt)
+		sgt.dropped = true
+		return
+	case actionLog:
+		if !sgt.dropped {
+			f.action.(*trapLogger).processTrap(sgt)
 		}
-	default:
-		trapexLog.Warn().Int("action_type", f.actionType).Msg("Unkown action type given to processAction")
+	case actionLogBreak:
+		if !sgt.dropped {
+			f.action.(*trapLogger).processTrap(sgt)
+		}
+		sgt.dropped = true
+		return
+	case actionCsv:
+		if !sgt.dropped {
+			f.action.(*trapCsvLogger).processTrap(sgt)
+		}
+	case actionCsvBreak:
+		if !sgt.dropped {
+			f.action.(*trapCsvLogger).processTrap(sgt)
+		}
+		sgt.dropped = true
+		return
 	}
-	if f.BreakAfter {
-		trap.Dropped = true
-	}
-	return err
 }
